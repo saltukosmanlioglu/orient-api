49b105baddfc9063b442320a90f10bc8ee8dba90
OSMANLIOGLU <osmanlioglu@OSMANLIOGLU-MacBook-Air.local>
2022-03-05 19:57:44 +0300

    fix

--- @ooic/core/app.ts
+++ @ooic/core/app.ts
@@ -19,6 +19,7 @@ export async function ooic(config: OoicConfig) {
   config.morgan?.enabled && process.env.NODE_ENV === "development" && app.use(morgan(config.morgan.format, config.morgan.options));
   config.cookieParser?.enabled && app.use(cookieParser(config.cookieParser.secret, config.cookieParser.options));
   app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'))
   app.use(
     fileUpload({
       useTempFiles: true,
--- src/controller/file/methods/upload/upload.ts
+++ src/controller/file/methods/upload/upload.ts
@@ -11,11 +11,11 @@ const upload: RequestHandler = async (request, response, next) => {
 
     request.files.uploadFile.mv(uploadPath, (err) => {
       if (err) {
-        console.log(err)
+        console.log(err);
         throw { statusCode: 500, message: "Dosya yüklenirken bir hata oluştu." };
       }
 
-      response.status(200).send(uploadPath);
+      response.status(200).send({ uploadPath });
     });
   } catch (error) {
     next(error);
