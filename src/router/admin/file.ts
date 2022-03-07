2ffa0c1501e5ca009a19cbcdc460d1d946a486c6
hakanozoglu <hakan.ozoglu@grapecity.com>
2022-03-07 17:13:03 +0300

    package style core

--- src/router/admin/file.ts
+++ src/router/admin/file.ts
@@ -1,10 +1,10 @@
-import { express, Router } from "@ooic/core";
+import { express, Router, fileUpload } from "@ooic/core";
 import * as file from "@/controller/file";
 import * as auth from "@/controller/auth";
 
 const router = Router();
 
 router.use("/serve", express.static("uploads"));
-router.post("/upload", file.upload);
+router.post("/upload", fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }), file.upload);
 
 export default router;
