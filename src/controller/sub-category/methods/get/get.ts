aa1fc981afd0ac1e0401e60158126ca031341e49
OSMANLIOGLU <osmanlioglu@OSMANLIOGLU-MacBook-Air.local>
2022-03-12 17:56:40 +0300

    fix

--- src/controller/sub-category/methods/get/get.ts
+++ src/controller/sub-category/methods/get/get.ts
@@ -5,7 +5,8 @@ const get: RequestHandler = async (request, response, next) => {
   try {
     const categories = await SubCategory.findAll({
       order: [["createdAt", "desc"]],
-      include: [{ association: "category" }],
+      attributes: { exclude: ["categoryId"] },
+      include: [{ association: "category", attributes: ["title", "id"] }],
     });
     response.status(200).send(categories);
   } catch (error) {
