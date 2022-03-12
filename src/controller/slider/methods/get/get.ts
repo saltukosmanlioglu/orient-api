b27f603b5701ca3da9fe2423a3d639618b2c28fd
OSMANLIOGLU <osmanlioglu@OSMANLIOGLU-MacBook-Air.local>
2022-03-12 20:02:19 +0300

    fix

--- src/controller/slider/methods/get/get.ts
+++ src/controller/slider/methods/get/get.ts
@@ -1,13 +1,9 @@
 import { Slider } from "@/model/Slider";
 import { RequestHandler } from "@ooic/core";
-import { schema } from ".";
 
 const get: RequestHandler = async (request, response, next) => {
   try {
-    const { orderMode, orderBy } = schema.query.parse(request.query);
-
     const sliders = await Slider.findAll({
-      order: [[orderMode, orderBy]],
       attributes: { exclude: ["productId"] },
       include: [{ association: "product", attributes: ["title", "id"] }],
     });
