467b16aa663a94d490e0386a89571524f0f7fe03
OSMANLIOGLU <osmanlioglu@OSMANLIOGLU-MacBook-Air.local>
2022-03-12 18:35:45 +0300

    fix

--- src/controller/product/methods/get/get.ts
+++ src/controller/product/methods/get/get.ts
@@ -1,14 +1,9 @@
 import { Product } from "@/model/Product";
 import { RequestHandler } from "@ooic/core";
-import { schema } from ".";
 
 const get: RequestHandler = async (request, response, next) => {
   try {
-    const { orderMode, orderBy } = schema.query.parse(request.query);
-
-    const products = await Product.findAll({
-      order: [[orderBy, orderMode]],
-    });
+    const products = await Product.findAll();
     response.status(200).send(products);
   } catch (error) {
     next(error);
--- src/controller/product/methods/get/schema.ts
+++ src/controller/product/methods/get/schema.ts
@@ -2,6 +2,4 @@ import { zod } from "@ooic/core";
 
 export const query = zod.object({
   title: zod.string().optional(),
-  orderMode: zod.string().optional().default("asc"),
-  orderBy: zod.string().optional().default("createdAt"),
 });
