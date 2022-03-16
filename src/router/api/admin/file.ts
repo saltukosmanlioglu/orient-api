3306b1b4ab82226dcee91a6ee235b7f5fa87f998
OSMANLIOGLU <osmanlioglu@OSMANLIOGLU-MacBook-Air.local>
2022-03-16 03:52:24 +0300

    fix: serve

--- src/router/api/admin/file.ts
+++ src/router/api/admin/file.ts
@@ -4,7 +4,7 @@ import * as auth from "@/controller/auth";
 
 const router = Router();
 
-router.use("/serve", auth.verifyToken, express.static("uploads"));
+router.use("/serve", express.static("uploads"));
 router.post("/upload", auth.verifyToken, fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }), file.upload);
 
 export default router;
